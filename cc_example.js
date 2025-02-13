abe6bfd19ac287545ce9cfb666c30340a272854d
Lisbeth Arias Cabral <lariascabral@usf.edu>
2025-02-13 16:01:20 -0500

    Task 2 - Invenotry Management

--- cc_example.js
+++ cc_example.js
@@ -11,3 +11,31 @@ function findOrder(orders, orderId) {
 } ;
 
 console.log("Order ID result:", findOrder(orders, 102)) ;
+
+
+// Task 2
+
+let inventory = {
+    items: [],
+    addItem(name, quantity) {
+        this.items.push({name, quantity})
+    } ,
+    removeLatestItem() {
+        this.items.pop() ;
+    } ,
+
+    removeFirstItem() {
+        this.items.shift()
+    } ,
+} ;
+
+inventory.addItem("Monitor", 5) 
+inventory.addItem("Keyboard", 10) 
+inventory.addItem("Mouse", 3) ;
+console.log("Inventory:", JSON.stringify(inventory.items)) ;
+
+inventory.removeLatestItem() ;
+console.log("Updated Inventory:", JSON.stringify(inventory.items)) ;
+
+inventory.removeFirstItem() ;
+console.log("Final Inventory:", JSON.stringify(inventory.items)) ;
